2b20bedded114626c6b6d5b10b09c86a998a3a91
Seung Hyun Eun <seunghyuneun@Seungs-MacBook-Pro.local>
2018-02-13 20:59:01 -0800

    Initial commit. HelloWorld does nothing.

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Seung Hyun Eun on 2018. 2. 13..
+//  Copyright © 2018년 Sunny Eun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
